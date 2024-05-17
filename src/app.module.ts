02630c6269cb83aada99cce1e85877f568559373
Rafael V. de Oliveira <elrafael123@gmail.com>
2024-05-17 18:15:33 +0100

    jwt service for auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,20 @@ import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import { ConfigModule } from '@nestjs/config'
+import { APP_GUARD } from '@nestjs/core'
+import { AuthGuard } from './auth/guards/auth.guard'
+import { JwtService } from '@nestjs/jwt'
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    JwtService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
