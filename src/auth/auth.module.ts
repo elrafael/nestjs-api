b06dc838f24c950cfc76444a0571151a21b7cb97
Rafael V. de Oliveira <elrafael123@gmail.com>
2024-05-17 18:14:31 +0100

    improv cod

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { readFileSync } from 'fs'
+import { UsersModule } from 'src/users/users.module'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
-import { UsersModule } from 'src/users/users.module'
-import { JwtModule } from '@nestjs/jwt'
-import { LocalStrategy } from './strategies/local.strategy'
 import { JwtStrategy } from './strategies/jwt.strategy'
-import { PassportModule } from '@nestjs/passport'
-import * as fs from 'fs'
+import { LocalStrategy } from './strategies/local.strategy'
 
 @Module({
   imports: [
@@ -14,8 +14,8 @@ import * as fs from 'fs'
     PassportModule,
     JwtModule.registerAsync({
       useFactory: () => ({
-        privateKey: fs.readFileSync(process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY, 'utf8'),
-        publicKey: fs.readFileSync(process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY, 'utf8'),
+        privateKey: readFileSync(process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY, 'utf8'),
+        publicKey: readFileSync(process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY, 'utf8'),
         signOptions: { expiresIn: '120m', algorithm: 'RS256' },
       }),
     }),
