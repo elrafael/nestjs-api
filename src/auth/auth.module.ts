0d6df18562cea5aeb85dd7a055cafd975e75fdc1
Rafael V. de Oliveira <elrafael123@gmail.com>
2024-05-17 14:06:40 +0100

    closes #11

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,16 +6,19 @@ import { JwtModule } from '@nestjs/jwt'
 import { LocalStrategy } from './strategies/local.strategy'
 import { JwtStrategy } from './strategies/jwt.strategy'
 import { PassportModule } from '@nestjs/passport'
+import * as fs from 'fs'
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: process.env.SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
-    }),
     PassportModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        privateKey: fs.readFileSync(process.env.JWT_ACCESS_TOKEN_PRIVATE_KEY, 'utf8'),
+        publicKey: fs.readFileSync(process.env.JWT_ACCESS_TOKEN_PUBLIC_KEY, 'utf8'),
+        signOptions: { expiresIn: '120m', algorithm: 'RS256' },
+      }),
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy, JwtStrategy],
