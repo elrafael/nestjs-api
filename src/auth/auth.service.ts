ea9c3e7944955cf676d5ddec373136f909205325
Rafael V. de Oliveira <elrafael123@gmail.com>
2024-05-18 19:36:13 +0100

    validate user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -18,11 +18,6 @@ export class AuthService {
   }
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const user = await this.usersService.findOne(username)
-    if (user && user.password === pass) {
-      const { password, ...result } = user
-      return result
-    }
-    return null
+    return this.usersService.validateLogin(username, pass)
   }
 }
