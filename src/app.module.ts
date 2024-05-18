a829d62cb4cae925341d7eb2b98626d16b2a5f06
Rafael V. de Oliveira <elrafael123@gmail.com>
2024-05-18 19:34:49 +0100

    typeorm mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,28 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
-import { ConfigModule } from '@nestjs/config'
-import { APP_GUARD } from '@nestjs/core'
-import { AuthGuard } from './auth/guards/auth.guard'
-import { JwtService } from '@nestjs/jwt'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    JwtService,
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_URL,
+      database: process.env.MONGODB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    AuthModule,
+    UsersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService, JwtService],
 })
 export class AppModule {}
