78d3931fabbc10e771537f24cca55a040f8dc526
Rafael V. de Oliveira <elrafael123@gmail.com>
2025-01-29 11:44:49 +0000

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Ciao World Teste'
+    return 'Ciao World Teste from github'
   }
 }
