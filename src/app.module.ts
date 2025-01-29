59f7b4f27b62edd3db109b28b1f07dff9ea05318
Rafael V. de Oliveira <rafael_oliveira@ext.nowo.pt>
2025-01-29 10:52:52 +0000

    feat: remove temp db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,28 +2,27 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { APP_GUARD } from '@nestjs/core'
 import { JwtService } from '@nestjs/jwt'
-import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
 import { AuthGuard } from './auth/guards/auth.guard'
 import { RolesGuard } from './auth/guards/roles.guard'
-import { UsersModule } from './users/users.module'
 import { RolesModule } from './roles/roles.module'
+import { UsersModule } from './users/users.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: 3306,
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.MYSQL_HOST,
+    //   port: 3306,
+    //   username: process.env.MYSQL_USERNAME,
+    //   password: process.env.MYSQL_PASSWORD,
+    //   database: process.env.MYSQL_DATABASE,
+    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     AuthModule,
     UsersModule,
     RolesModule,
